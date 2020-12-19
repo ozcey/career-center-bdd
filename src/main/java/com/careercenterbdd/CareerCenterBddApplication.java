7d48856e8b229dc1414230af22b48cdbbca36be6
Oz <ozcancey20@gmail.com>
2020-12-18 21:34:21 -0500

    created project

---
+++ src/main/java/com/careercenterbdd/CareerCenterBddApplication.java
@@ -0,0 +1,13 @@
+package com.careercenterbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCenterBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerCenterBddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careercenterbdd/CareerCenterBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careercenterbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCenterBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
